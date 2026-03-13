62e21cd7cf5ce4af004751c5c33b7c6a37664219
Guillem Gual <guillemgual@paucasesnovescifp.cat>
2026-03-13 09:03:08 +0100

    Base de la pàgina jugar feta

--- js/jugar.js
+++ js/jugar.js
@@ -1,4 +1,14 @@
 
-function jugar() {
-    window.location.href = "link a la pàgina de jugar";
+function redireccioPagina(pagina) {
+    
+    switch (pagina) {
+        case 'jugar':
+            window.location.href = 'pagina del joc';
+            break;
+        case 'controls':
+            window.location.href = '../Controls/index.html';
+            break;
+        default:
+            console.error('Pàgina no reconeguda: ' + pagina);
+    }
 }
