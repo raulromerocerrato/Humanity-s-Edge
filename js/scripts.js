f9729252299ba090af6cbc24b5231a8e30b80979
Raúl Romero Cerrato <raulromero@paucasesnovescifp.cat>
2026-03-13 09:30:26 +0100

    connexió amb scripts.js i eliminació dels antics arxius js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+function redireccioPagina(pagina) {
+    
+    switch (pagina) {
+        case 'jugar':
+            window.location.href = 'pagina del joc';
+            break;
+        case 'controls':
+            window.location.href = '../Controls/index.html';
+            break;
+        default:
+            console.error('Pàgina no reconeguda: ' + pagina);
+    }
+}
+
 function navPagina(nomPagina){
     switch(nomPagina){
         case "Historia":
