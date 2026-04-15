a008d2e4a6c11469b5e7076a49c680d2c513371b
Jeremy Hernández <jeremysamuelhernandez@paucasesnovescifp.cat>
2026-04-15 09:37:06 +0200

    cambio diseño color

--- js/scripts.js
+++ js/scripts.js
@@ -218,15 +218,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (localStorage.getItem('modo') === 'light') {
         document.body.classList.add('light');
-        botonModo.textContent = '🌙';
+        botonModo.textContent = 'Claro';
     } else {
-        botonModo.textContent = '☀️';
+        botonModo.textContent = 'Oscuro';
     }
 
     botonModo.addEventListener('click', () => {
         document.body.classList.toggle('light');
         const esModoClaro = document.body.classList.contains('light');
-        botonModo.textContent = esModoClaro ? '🌙' : '☀️';
+        botonModo.textContent = esModoClaro ? 'Claro' : 'Oscuro';
         localStorage.setItem('modo', esModoClaro ? 'light' : 'dark');
     });
 });
